e38a1693a5b2a19464d43e79b0c9175af68a8e28
OlataUFMS <112577844+OlataUFMS@users.noreply.github.com>
2022-10-17 15:07:50 -0400

    Alterações em Jogada

--- src/base/jogador/Jogada.java
+++ src/base/jogador/Jogada.java
@@ -1,18 +1,17 @@
 package jogador;
 
-public class Jogada{
+public interface Jogada{
     /*Classe: jogada de compra;
         Função da classe: caso o jogador não detiver de nenhuma carta que combine com a que está na 
     mesa, ou outro jogador anterior lançar uma das cartas especiais (+2 ou +4), deve ser retirado uma 
     carta do baralho de compra e adicionada ao baralho do jogador em questão.*/  
+    public void comprar(int quant);
     
     /*Classe: jogada de descarte; 
         Função da classe: caso o jogador detiver de alguma carta que combine com a que foi jogada 
     anteriormente, a carta deve ser selecionada e retirada do baralho do jogador e colocada no baralho 
     de descarte, caso o jogador detiver de mais uma carta que combine com a da mesa, ele deve 
     selecionar um das duas. */
+    public void descartar();
 
-    /*Não foi escritro o codigo pois necessitariamos de mais informações
-     sobre o jogador, entre outros
-    */
 }
