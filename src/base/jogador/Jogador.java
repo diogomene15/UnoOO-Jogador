d991c57bcb32b957d4cdbba1d83129cade2f50b5
OlataUFMS <112577844+OlataUFMS@users.noreply.github.com>
2022-10-17 15:07:25 -0400

    Alterações em jogada e jogador

--- src/base/jogador/Jogador.java
+++ src/base/jogador/Jogador.java
@@ -1,9 +1,17 @@
 package base.jogador;
 
-public class Jogador {
+import java.util.ArrayList;
+import base.*;
+
+public class Jogador implements jogada{
 
     private String nome;
-    private MaoCartas maoCartas;
+<<<<<<< HEAD
+=======
+    //private MaoCartas maoCartas;
+>>>>>>> fe8d05f168bb1ffbcfc4a2698595bafa7acb9442
+
+    ArrayList<Carta> maoJogador = new ArrayList<Carta>();
 
     public String getNome() {
         return nome;
@@ -12,16 +20,43 @@ public class Jogador {
         this.nome = nome;
     }
 
-    public MaoCartas getMaoCartas() {
-        return maoCartas;
+    /**
+    *
+    * Inicializa a lista maoJogador, comprando 7 cartas
+    *
+    **/
+    private void inicialzarMao(){
+        comprar(7);
     }
-    public void setMaoCartas(MaoCartas maoCartas) {
-        this.maoCartas = maoCartas;
+
+    /**
+    *
+    * Compra uma carta, adicionando uma carta a MaoJogador,
+    * retirando-a do monte de compras
+    *
+    **/
+    @Override
+    public void comprar(int quant){
+        for(int i = 0; i<quant; i++){
+            //maojogador.add(carta do topo do monte de compras)
+            // Retirar carta do topo do monte de compras
+        }
+    }
+
+     /**
+    *
+    * Descarta uma carta, retirando uma carta a MaoJogador,
+    * adicionando-a ao monte de descarte
+    *
+    **/
+    @Override
+    public void descartar(){
+        //
     }
 
     public Jogador(String nome, MaoCartas maoInicial){
         this.nome = nome.toLowerCase().trim();
-        this.maoCartas = maoInicial;
+        this.inicialzarMao();
     }
     public Jogador(String nome){
         this.nome = nome.toLowerCase().trim();
