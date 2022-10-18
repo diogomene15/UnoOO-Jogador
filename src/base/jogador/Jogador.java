986fdd202c6fa67af50cff052a0c2afcbeab8435
diogomene <diogodeliamamenezes15@gmail.com>
2022-10-17 21:30:34 -0400

    Alterando forma de integração de classes 'Jogador' e 'MaoCartas'

--- src/base/jogador/Jogador.java
+++ src/base/jogador/Jogador.java
@@ -7,29 +7,48 @@ import base.cartas.Carta;
 public class Jogador implements Jogada{
 
     private String nome;
+    private MaoCartas maoJogador;
 
-    ArrayList<Carta> maoJogador = new ArrayList<Carta>();
+    // ArrayList<Carta> maoJogador = new ArrayList<Carta>();
 
+    /**
+     * Retorna o nome do jogador
+     * @return nome - nome atual do jogador
+     */
     public String getNome() {
         return nome;
     }
+
+    /**
+     * Altera o nome do jogador
+     * @param nome (nome a ser atualizado para o jogador)
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
     /**
     *
-    * Inicializa a lista maoJogador, comprando 7 cartas
-    *
+    * Inicializa a maoJogador a partir de uma lista de cartas
+    * @param cartasIniciais - Lista de cartas iniciais do jogador
+    * @see MaoCartas
     **/
-    private void inicialzarMao(){
-        comprar(7);
+    public void inicializarMao(ArrayList<Carta> cartasIniciais){
+        this.maoJogador = new MaoCartas(cartasIniciais);
     }
 
     /**
     *
-    * Compra n cartas, adicionando uma carta a MaoJogador
+    * Inicializa a maoJogador sem nenhuma carta
+    * @see MaoCartas
+    **/
+    private void inicializarMao(){
+        this.maoJogador = new MaoCartas();
+    }
+    /**
     *
+    * Compra n cartas, adicionando as cartas na MaoJogador
+    * @deprecated - deve ser consultado futaramente com Jogo, ou Roda.
     **/
     @Override
     public void comprar(int quant){
@@ -56,13 +75,33 @@ public class Jogador implements Jogada{
     public void descartar(){
         //
     }
-
+    /**
+     * Retorna a quantidade de cartas atual do jogador
+     * @return Quantidade de cartas que o jogador possui atualmente
+     */
+    public int getQuantidadeCartas(){
+        return 0;
+    }
+    /**
+     * Construtor que recebe um nome para o jogador e um objeto de MaoCartas
+     * @see MaoCartas
+     * @param nome
+     * @param maoInicial
+     */
     public Jogador(String nome, MaoCartas maoInicial){
         this.nome = nome.toLowerCase().trim();
-        this.inicialzarMao();
+        this.maoJogador = maoInicial;
     }
+    /**
+     * Construtor que recebe o nome do jogador
+     * e inicializa uma maoJogador (objeto de MaoCartas) sem
+     * nenhuma carta.
+     * @param nome - Nome inicial do jogador
+     * @see MaoCartas
+     */
     public Jogador(String nome){
         this.nome = nome.toLowerCase().trim();
+        this.inicializarMao();
     }
 
 }
--- src/base/jogador/MaoCartas.java
+++ src/base/jogador/MaoCartas.java
@@ -11,21 +11,45 @@ public class MaoCartas extends Baralho{
     private ArrayList<Carta> cartas;
     public static final short tamInicial = 7;
     
-    public void inicializar(ArrayList<Carta> cartasIniciais){
+    /**
+     * Realiza a inicialização do objeto de MaoCartas
+     * @param cartasIniciais - Lista de cartas iniciais de MaoCartas
+     */
+    private void inicializar(ArrayList<Carta> cartasIniciais){
         this.cartas = cartasIniciais;
     }
 
+    /**
+     * Realiza a adição de uma carta na lista de cartas
+     * atuais no objeto de MaoCartas
+     * @param carta - Carta a ser adicionada em MaoCartas
+     */
     public void receberCarta(Carta carta){
         this.cartas.add(carta);
     }
-
+    /**
+     * Retorna a quantidade atual de cartas na lista de cartas
+     * de MaoCartas
+     * @return
+     */
     public int getQuantidadeCartas() {
         return this.cartas.size();
     }
-
+    /**
+     * Construtor que recebe uma lista inicial de cartas para
+     * a MaoCartas
+     * @param cartasIniciais - Lista de cartas iniciais para MaoCartas
+     */
     public MaoCartas(ArrayList<Carta> cartasIniciais){
         super(Baralho.NORMAL);
         this.inicializar(cartasIniciais);
+    }
+    /**
+     * Construtor que retorna uma MaoCartas totalmente vazia
+     * (sem cartas).
+     */
+    public MaoCartas(){
+        super(Baralho.NORMAL);
     }
 
 }
