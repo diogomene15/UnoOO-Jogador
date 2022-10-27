c6f93d4e05c185685c9b351ae7a5a6d8282e1e41
diogomene <diogodeliamamenezes15@gmail.com>
2022-10-26 22:14:56 -0400

    Initial commit of docs

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    lang:"pt-BR",
+    title:"Jogador Docs",
+    appearance:"dark",
+    themeConfig:{
+        nav:[
+            {text:"Início", link:"/"},
+            {text:"Classes",
+                items:[
+                    {text:"Jogador", link:"/jogador"},
+                    {text:"Mão Cartas", link:"/mao-cartas"},
+                    {text:"Jogada", link:"/jogada"}    
+                ]
+            }
+
+
+        ],
+
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/diogomene15/UnoOO-Jogador/' }
+        ],
+
+        sidebar:[
+            {
+                text:"Classes",
+                items:[
+                    {text:"Jogador", link:"/jogador"},
+                    {text:"Mão Cartas", link:"/mao-cartas"},
+                    {text:"Jogada", link:"/jogada"}   
+                ]
+            }
+        ],
+        
+        docFooter: {
+            prev: 'Página anterior',
+            next: 'Próxima página'
+        }
+    }
+}
---
+++ docs/index.md
@@ -0,0 +1,4 @@
+# Bem-vindo!
+Olá, você acaba de achar uma documentação temporária deste grupo.
+
+Aqui se encontraram algumas documentações das classes [Jogador](/jogador), [Jogada](/jogada) e [MaoCartas](/maoCartas)
