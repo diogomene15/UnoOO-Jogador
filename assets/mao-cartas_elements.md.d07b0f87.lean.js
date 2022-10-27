9f429552298fd051ca757953298bd3d87f108aac
diogomene15 <diogomene15@users.noreply.github.com>
2022-10-27 22:54:32 +0000

    deploy: dade813c13d84f7cf32022ed0fd00efe1b127a6d

---
+++ assets/mao-cartas_elements.md.d07b0f87.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.24cb7945.js";const i=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"mao-cartas/elements.md"}'),s={name:"mao-cartas/elements.md"};function r(o,n,c,m,p,_){return a(),t("div")}const l=e(s,[["render",r]]);export{i as __pageData,l as default};
